d32eda0fa8b65d02ff1218e005da4b420ed3b552
mohamed.elkhodary <mohamed.elkhodary@itworx.com>
2015-11-21 14:52:26 +0200

    Add solutions

---
+++ Mail/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Office365Mail.Startup))]
+
+namespace Office365Mail {
+  public partial class Startup {
+
+    public void Configuration(IAppBuilder app)
+    {
+      ConfigureAuth(app);
+    }
+
+  }
+}
